97fc14d52c4e3afaf9bf9a4fc7c36b3cbf809ed7
alex8616 <aledar16@gmail.com>
2025-06-07 01:39:06 -0400

    Correccion dockerfile

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,7 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -276,8 +277,9 @@
       </div>
     </footer>
     <!-- Bootstrap Core JavaScript -->
-    <script src="{{ asset('js/jquery-1.11.1.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="/js/jquery-1.11.1.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+
 
     <script>
 			
